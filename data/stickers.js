c9bb549bc19ce515d1152ed062f51b58b682e721
Sonika303 <sonikakukreti05@gmail.com>
2026-04-07 22:07:10 +0530

    eeeeeeeee

--- data/stickers.js
+++ data/stickers.js
@@ -10,7 +10,8 @@ const STICKERS = [
   { id: 21, name: "Kurama 1",  desc: "Kurama the Nine-Tailed Fox Beast",                image: "images/anime/kurama1.png",  style: "anime",        price: 10 },
   { id: 22, name: "L Lawliet 1",  desc: "Chibi L Lawliet with his distinctive hairstyle",   image: "images/anime/LLawliet1.png",  style: "anime",        price: 10 },
   { id: 23, name: "Itachi 1",    desc: "Funny Itachi Uchiha Pose", image: "images/anime/itachi1.png",    style: "anime",        price: 10 },
-   
+  { id: 24, name: "Itachi 2",   desc: "itachi sitting pose and itachi background behind him", image: "images/anime/itachi2.png",   style: "anime",        price: 10 },  
+ 
   /* ── HELLO KITTY ── */
   { id: 2,  name: "Hello Kitty 1", desc: "Hello Kitty standing",                                    image: "images/HelloKitty/HelloKitty1.png", style: "hellokitty", price: 10 },
   { id: 3,  name: "Hello Kitty 2", desc: "Adorable Hello Kitty peeking from behind a ribbon",       image: "images/HelloKitty/HelloKitty2.png", style: "hellokitty", price: 10 },
@@ -19,14 +20,14 @@ const STICKERS = [
 
   /* ── HARRY POTTER ── */
   { id: 6,  name: "Harry Potter 1",    desc: "Chibi Harry Potter holding his wand with iconic glasses",     image: "images/harrypotter/HarryPotter1.png",    style: "harrypotter", price: 10 },
-  { id: 7,  name: "Hermione Granger",  desc: "Chibi Hermione Granger with her characteristic bushy hair",   image: "images/harrypotter/HermioneGranger1.png", style: "harrypotter", price: 10 },
-  { id: 8,  name: "Ron Weasley",       desc: "Chibi Ron Weasley with his red hair and freckles",             image: "images/harrypotter/RonWeasley1.png",      style: "harrypotter", price: 10 },
-  { id: 9,  name: "Rubeus Hagrid",     desc: "Chibi Hagrid with his characteristic beard and kind eyes",     image: "images/harrypotter/RubeusHagrid1.png",    style: "harrypotter", price: 10 },
-  { id: 10, name: "Severus Snape",     desc: "Chibi Snape with his characteristic scowl and dark hair",      image: "images/harrypotter/SeverusSnape1.png",    style: "harrypotter", price: 10 },
-  { id: 11, name: "Albus Dumbledore",  desc: "Chibi Dumbledore with long beard and half-moon glasses",       image: "images/harrypotter/AlbusDumbledore1.png", style: "harrypotter", price: 10 },
-  { id: 12, name: "Draco Malfoy",      desc: "Chibi Draco with slicked-back blonde hair and sly expression", image: "images/harrypotter/DracoMalfoy1.png",     style: "harrypotter", price: 10 },
-  { id: 13, name: "Luna Lovegood",     desc: "Chibi Luna with her distinctive hair and dreamy eyes",          image: "images/harrypotter/LunaLovegood1.png",    style: "harrypotter", price: 10 },
-  { id: 14, name: "Sorting Hat",       desc: "Chibi Sorting Hat with its distinctive shape and wise face",    image: "images/harrypotter/SortingHat1.png",      style: "harrypotter", price: 10 },
-  { id: 15, name: "Harry Potter Logo", desc: "Harry Potter iconic lightning bolt logo",                       image: "images/harrypotter/HarryPotterLogo1.png", style: "harrypotter", price: 10 },
+  { id: 7,  name: "Hermione Grange 1",  desc: "Chibi Hermione Granger with her characteristic bushy hair",   image: "images/harrypotter/HermioneGranger1.png", style: "harrypotter", price: 10 },
+  { id: 8,  name: "Ron Weasley 1",       desc: "Chibi Ron Weasley with his red hair and freckles",             image: "images/harrypotter/RonWeasley1.png",      style: "harrypotter", price: 10 },
+  { id: 9,  name: "Rubeus Hagrid 1",     desc: "Chibi Hagrid with his characteristic beard and kind eyes",     image: "images/harrypotter/RubeusHagrid1.png",    style: "harrypotter", price: 10 },
+  { id: 10, name: "Severus Snape 1",     desc: "Chibi Snape with his characteristic scowl and dark hair",      image: "images/harrypotter/SeverusSnape1.png",    style: "harrypotter", price: 10 },
+  { id: 11, name: "Albus Dumbledore 1",  desc: "Chibi Dumbledore with long beard and half-moon glasses",       image: "images/harrypotter/AlbusDumbledore1.png", style: "harrypotter", price: 10 },
+  { id: 12, name: "Draco Malfoy 1",      desc: "Chibi Draco with slicked-back blonde hair and sly expression", image: "images/harrypotter/DracoMalfoy1.png",     style: "harrypotter", price: 10 },
+  { id: 13, name: "Luna Lovegood 1",     desc: "Chibi Luna with her distinctive hair and dreamy eyes",          image: "images/harrypotter/LunaLovegood1.png",    style: "harrypotter", price: 10 },
+  { id: 14, name: "Sorting Hat 1",       desc: "Chibi Sorting Hat with its distinctive shape and wise face",    image: "images/harrypotter/SortingHat1.png",      style: "harrypotter", price: 10 },
+  { id: 15, name: "Harry Potter Logo 1", desc: "Harry Potter iconic lightning bolt logo",                       image: "images/harrypotter/HarryPotterLogo1.png", style: "harrypotter", price: 10 },
 
 ];
