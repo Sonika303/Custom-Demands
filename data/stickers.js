a0b01845f8464d71f24ae8c13e7d2e59feff4dd5
Sonika303 <sonikakukreti05@gmail.com>
2026-04-07 16:29:07 +0530

    eeeeeee

--- data/stickers.js
+++ data/stickers.js
@@ -6,7 +6,7 @@ const STICKERS = [
     desc:  "Pain Staring",
     image: "images/anime/pain1.png",
     style: "anime",
-    price: 15
+    price: 10
   },
 
   {
@@ -15,7 +15,7 @@ const STICKERS = [
     desc:  "Hello Kitty Standing",
     image: "images/HelloKitty/HelloKitty1.png",
     style: "hellokitty",
-    price: 15
+    price: 10
   },
 
   {
@@ -24,7 +24,7 @@ const STICKERS = [
     desc:  "Adorable Hello Kitty peeking out from behind a ribbon",
     image: "images/HelloKitty/HelloKitty2.png",
     style: "hellokitty",
-    price: 15
+    price: 10
   },
 
   {
@@ -33,7 +33,7 @@ const STICKERS = [
     desc:  "Hello Kitty head with a cute red bow",
     image: "images/HelloKitty/HelloKitty3.png",
     style: "hellokitty",
-    price: 15
+    price: 10
   },
 
   {
@@ -42,7 +42,7 @@ const STICKERS = [
     desc:  "Hello Kitty sitting and holding a strawberry, looking adorable and sweet",
     image: "images/HelloKitty/HelloKitty4.png",
     style: "hellokitty",
-    price: 15
+    price: 10
   },
 
   {
@@ -51,7 +51,7 @@ const STICKERS = [
     desc:  "Chibi Harry Potter holding his wand and wearing his iconic glasses",
     image: "images/harrypotter/HarryPotter1.png",
     style: "harrypotter",
-    price: 15
+    price: 10
   },
 
   {
@@ -60,7 +60,7 @@ const STICKERS = [
     desc:  "Chibi Hermione Granger with her characteristic bushy hair",
     image: "images/harrypotter/HermioneGranger1.png",
     style: "harrypotter",
-    price: 15
+    price: 10
   },
 
   {
@@ -69,7 +69,7 @@ const STICKERS = [
     desc:  "Chibi Ron Weasley with his red hair and freckles",
     image: "images/harrypotter/RonWeasley1.png",
     style: "harrypotter",
-    price: 15
+    price: 10
   },
 
   {
@@ -78,7 +78,7 @@ const STICKERS = [
     desc:  "Chibi Rubeus Hagrid with his characteristic beard and kind eyes",
     image: "images/harrypotter/RubeusHagrid1.png",
     style: "harrypotter",
-    price: 15
+    price: 10
   },
 
   {
@@ -87,7 +87,7 @@ const STICKERS = [
     desc:  "Chibi Severus Snape with his characteristic scowl and dark hair",
     image: "images/harrypotter/SeverusSnape1.png",
     style: "harrypotter",
-    price: 15
+    price: 10
   },
 
   {
@@ -96,7 +96,7 @@ const STICKERS = [
     desc:  "Chibi Albus Dumbledore with his long beard and half-moon glasses",
     image: "images/harrypotter/AlbusDumbledore1.png",
     style: "harrypotter",
-    price: 15
+    price: 10
   },
 
   {
@@ -105,7 +105,7 @@ const STICKERS = [
     desc:  "Chibi Draco Malfoy with his slicked-back blonde hair and sly expression",
     image: "images/harrypotter/DracoMalfoy1.png",
     style: "harrypotter",
-    price: 15
+    price: 10
   },
 
   {
@@ -114,7 +114,7 @@ const STICKERS = [
     desc:  "Chibi Luna Lovegood with her distinctive hair and dreamy eyes with her glasses",
     image: "images/harrypotter/LunaLovegood1.png",
     style: "harrypotter",
-    price: 15
+    price: 10
   },
 
   {
@@ -123,33 +123,43 @@ const STICKERS = [
     desc:  "Chibi Sorting Hat with its distinctive shape and wise expression on its face",
     image: "images/harrypotter/SortingHat1.png",
     style: "harrypotter",
-    price: 15
+    price: 10
   },
 
   {
-    id:    15,
+    id:    10,
+    name:  "Harry Potter Logo 1",
+    desc:  "Harry Potter Logo",
+    image: "images/harrypotter/HarryPotterLogo1.png",
+    style: "harrypotter",
+    price: 10
+  },
+
+
+  {
+    id:    16,
     name:  "Luffy 1",
     desc:  "Luffy fist in the air friendship symbol",
     image: "images/anime/luffy1.png",
     style: "anime",
-    price: 15
-  },
-
-  {
-    id:    16,
-    name:  "Hakari 1",
-    desc:  "Hakari doing his domain expansion pose",
-    image: "images/anime/hakari1.png",
-    style: "anime",
-    price: 15
+    price: 10
   },
 
   {
     id:    17,
+    name:  "Hakari 1",
+    desc:  "Hakari doing his domain expansion pose",
+    image: "images/anime/hakari1.png",
+    style: "anime",
+    price: 10
+  },
+
+  {
+    id:    18,
     name:  "Gojo 1",
     desc:  "Gojo Satoru Head with his sunglasses",
     image: "images/anime/gojo1.png",
     style: "anime",
-    price: 15
+    price: 10
   }
 ];
