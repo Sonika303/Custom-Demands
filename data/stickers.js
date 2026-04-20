844040f570a27cbd64a90abf87b9f7fcce2575e4
Sonika303 <sonikakukreti05@gmail.com>
2026-04-20 20:21:03 +0530

    e

--- data/stickers.js
+++ data/stickers.js
@@ -4,22 +4,25 @@ const STICKERS = [
   { id: 1,  name: "Pain 1",    desc: "Pain Staring",                                   image: "images/anime/pain1.png",    style: "anime",        price: 10 },
   { id: 16, name: "Luffy 1",   desc: "Luffy fist in the air friendship symbol",         image: "images/anime/luffy1.png",   style: "anime",        price: 10 },
   { id: 17, name: "Hakari 1",  desc: "Hakari doing his domain expansion pose",          image: "images/anime/hakari1.png",  style: "anime",        price: 10 },
-  { id: 18, name: "Gojo 1",    desc: "Gojo Satoru head with his sunglasses",            image: "images/anime/gojo1.png",    style: "anime",        price: 10 },
-  { id: 19, name: "Gojo 2",    desc: "Gojo Satoru running with blindfold and arms out", image: "images/anime/gojo2.png",    style: "anime",        price: 10 },
+  { id: 18, name: "Gojo 1",    desc: "Gojo Satoru head with his sunglasses",            image: "images/anime/gojo/gojo1.png",    style: "anime",        price: 10 },
+  { id: 19, name: "Gojo 2",    desc: "Gojo Satoru running with blindfold and arms out", image: "images/anime/gojo/gojo2.png",    style: "anime",        price: 10 },
   { id: 20, name: "Naruto 1",  desc: "Naruto Uzumaki in a low-crouching position",      image: "images/anime/naruto1.png",  style: "anime",        price: 10 },
   { id: 21, name: "Kurama 1",  desc: "Kurama the Nine-Tailed Fox Beast",                image: "images/anime/kurama1.png",  style: "anime",        price: 10 },
   { id: 22, name: "L Lawliet 1",  desc: "Chibi L Lawliet with his distinctive hairstyle",   image: "images/anime/LLawliet1.png",  style: "anime",        price: 10 },
   { id: 23, name: "Itachi 1",    desc: "Funny Itachi Uchiha Pose", image: "images/anime/itachi1.png",    style: "anime",        price: 10 },
   { id: 24, name: "Itachi 2",   desc: "itachi sitting pose and itachi background behind him", image: "images/anime/itachi2.png",   style: "anime",        price: 10 },  
-  { id: 210, name: "Goku 1",   desc: "Goku Standing with pose and angry face expressions", image: "images/anime/goku1.png",   style: "anime",        price: 10 },
-  { id: 26, name: "Zoro 1",   desc: "Zoro standing with his sword", image: "images/anime/zoro1.png",   style: "anime",        price: 10 },
-  { id: 27, name: "Zoro 2",   desc: "Zoro posing and standing with his swords", image: "images/anime/zoro2.png",   style: "anime",        price: 10 },   
-  
+  { id: 25, name: "Goku 1",   desc: "Goku Standing with pose and angry face expressions", image: "images/anime/goku1.png",   style: "anime",        price: 10 },
+  { id: 26, name: "Zoro 1",   desc: "Zoro standing with his sword", image: "images/anime/zoro/zoro1.png",   style: "anime",        price: 10 },
+  { id: 27, name: "Zoro 2",   desc: "Zoro posing and standing with his swords", image: "images/anime/zoro/zoro2.png",   style: "anime",        price: 10 },   
+  { id: 28, name: "Gojo 3",   desc: "Chibi Gojo with a cute smile", image: "images/anime/gojo/gojo3.png",   style: "anime",        price: 10 },
+  { id: 29, name: "Sasuke 1",   desc: "Sasuke posing and holding his sword", image: "images/anime/sasuke/sasuke1.png",   style: "anime",        price: 10 },
+  { id: 30, name: "Sasuke 2",   desc: "Kid Sasuke wearing his headband with clan symbol and a serious expression doing jutsu pose", image: "images/anime/sasuke/sasuke2.png",   style: "anime",        price: 10 },
+
   /* ── HELLO KITTY ── */
   { id: 2,  name: "Hello Kitty 1", desc: "Hello Kitty standing",                                    image: "images/HelloKitty/HelloKitty1.png", style: "hellokitty", price: 10 },
   { id: 3,  name: "Hello Kitty 2", desc: "Adorable Hello Kitty peeking from behind a ribbon",       image: "images/HelloKitty/HelloKitty2.png", style: "hellokitty", price: 10 },
   { id: 4,  name: "Hello Kitty 3", desc: "Hello Kitty head with a cute red bow",                    image: "images/HelloKitty/HelloKitty3.png", style: "hellokitty", price: 10 },
-  { id: 10,  name: "Hello Kitty 4", desc: "Hello Kitty sitting and holding a strawberry",            image: "images/HelloKitty/HelloKitty4.png", style: "hellokitty", price: 10 },
+  { id: 5,  name: "Hello Kitty 4", desc: "Hello Kitty sitting and holding a strawberry",            image: "images/HelloKitty/HelloKitty4.png", style: "hellokitty", price: 10 },
 
   /* ── HARRY POTTER ── */
   { id: 6,  name: "Harry Potter 1",    desc: "Chibi Harry Potter holding his wand with iconic glasses",     image: "images/harrypotter/HarryPotter1.png",    style: "harrypotter", price: 10 },
@@ -31,6 +34,6 @@ const STICKERS = [
   { id: 12, name: "Draco Malfoy 1",      desc: "Chibi Draco with slicked-back blonde hair and sly expression", image: "images/harrypotter/DracoMalfoy1.png",     style: "harrypotter", price: 10 },
   { id: 13, name: "Luna Lovegood 1",     desc: "Chibi Luna with her distinctive hair and dreamy eyes",          image: "images/harrypotter/LunaLovegood1.png",    style: "harrypotter", price: 10 },
   { id: 14, name: "Sorting Hat 1",       desc: "Chibi Sorting Hat with its distinctive shape and wise face",    image: "images/harrypotter/SortingHat1.png",      style: "harrypotter", price: 10 },
-  { id: 110, name: "Harry Potter Logo 1", desc: "Harry Potter iconic lightning bolt logo",                       image: "images/harrypotter/HarryPotterLogo1.png", style: "harrypotter", price: 10 },
+  { id: 15, name: "Harry Potter Logo 1", desc: "Harry Potter iconic lightning bolt logo",                       image: "images/harrypotter/HarryPotterLogo1.png", style: "harrypotter", price: 10 },
 
 ];
