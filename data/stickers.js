8bf156f1b7d3da324d66d264babba023ef553eea
Sonika303 <sonikakukreti05@gmail.com>
2026-04-22 15:15:48 +0530

    e

--- data/stickers.js
+++ data/stickers.js
@@ -41,5 +41,7 @@ const STICKERS = [
   { id: 15, name: "Harry Potter Logo 1",desc: "Harry Potter iconic lightning bolt logo",                       image: "images/harrypotter/HarryPotterLogo1.png", style: "harrypotter", price: 10, inStock: true  },
 
   /* ── MARVEL ── */
-  { id: 31, name: "Spider-Man 1",  desc: "Spider-Man Hanging in the Air with the web",            image: "images/marvel/spiderman1.png",  style: "marvel", price: 10, inStock: true  },
+   { id: 31, name: "Spider-Man 1",  desc: "Spider-Man Hanging in the Air with the web",            image: "images/marvel/spiderman1.png",  style: "marvel", price: 10, inStock: true  },
+   { id: 36, name: "Iron Man 1",    desc: "Iron Man in his iconic red and gold suit flying", image: "images/marvel/ironman1.png",    style: "marvel", price: 10, inStock: true  },
+ 
 ];
